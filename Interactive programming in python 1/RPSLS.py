072096d2683c1e27477dd9fb5ddd5fb77bdada68
Ramya <ramyap09@gmail.com>
2017-12-01 17:51:50 -0800

    rock-paper-scissors-lizard-Spock game

--- Interactive programming in python 1/RPSLS.py
+++ Interactive programming in python 1/RPSLS.py
@@ -1,6 +1,9 @@
 import random
 
 def name_to_number(name):
+    """
+    Returns a unique number for each name, given the name as input.
+    """
     if name == "rock":
         return 0
     elif name == "Spock":
@@ -17,6 +20,9 @@ def name_to_number(name):
 
 
 def number_to_name(number):
+    """
+    Returns the name coresponding the number, given the number as input
+    """
     if number == 0:
         return "rock"
     elif number == 1:
@@ -32,10 +38,12 @@ def number_to_name(number):
 
 
 def rpsls(player_choice):
+    """
+    Given a name, this function prints out who wins the game (you or the computer) based on the the game rules
+    """
     player_number = name_to_number(player_choice)
     comp_number = random.randrange(0,5)
     print("Player chooses {}".format(number_to_name(player_number)))
-    #print("Player chooses %s" %(number_to_name(player_number)))
     print("Computer chooses {}".format(number_to_name(comp_number)))
     diff = (player_number - comp_number) % 5
     if diff == +1 or diff == +2:
@@ -48,6 +56,9 @@ def rpsls(player_choice):
         print("Try again")
 
 
+
+# Here , it keeps asking for the input, until a valid input is typed in.
+# The valid inputs are "rock", "Spock", "paper", "lizard", "scissors" - case sensitive
 while True:
     x = input("Enter your choice(rock/Spock/paper/lizard/scissors): ")
     if x not in ["rock", "paper", "lizard", "scissors", "Spock"]:
