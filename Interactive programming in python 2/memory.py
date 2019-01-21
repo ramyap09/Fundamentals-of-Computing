6dcde9ff33d0b3fa555f4d33d6a0a87ca16562e7
Ramya <ramyap09@gmail.com>
2019-01-21 12:07:39 -0800

    modifications

--- Interactive programming in python 2/memory.py
+++ Interactive programming in python 2/memory.py
@@ -28,12 +28,12 @@ def reset():
     """
      This function initializes all the list variables and other variables and hence starts new game.
     """
-    global exposed, x, text_id, nlist, state, turns, lst
+    global exposed, clicked_num_index_lst, text_id, clicked_num_lst, state, turns, lst
     exposed = [False]*16
-    x = []
+    clicked_num_index_lst = []
     canvas1.delete('numbers')  # Here deleted all the objects with the tag "numbers"
     text_id=[]
-    nlist=[]
+    clicked_num_lst=[]
     state=0
     turns=0
     label_turns.config(text = "Turns = 0 ")
@@ -55,44 +55,42 @@ def click(event):
     """
      This function executes the game logic each time there is a click on the cards.
     """
-    global exposed, state, x, turns, lst, nlist
+    global exposed, state, clicked_num_index_lst, turns, lst, clicked_num_lst
     # If we click exactly on the already displayed number on the card, .find_closest() will find the number object instead of the
     #rectanlge(ie., the card) leading to error. So we put it as try-except. clicking on the number wont do do anything to the
     # game except the error.
     try:
-        c = canvas1.find_closest(event.x,event.y)[0]
+        c = canvas1.find_closest(event.x,event.y)[0]  # Here we get to know which box is clicked (c is the id of the box clicked)
         #print(canvas1.gettags(c))
-        m = canvas1.coords(c)
-        #print(exposed)
+        m = canvas1.coords(c)   # m is the coodinates of the box clicked
         if exposed[c-1] == False:
             t_id = canvas1.create_text(m[0]+25, m[1]+50, fill = "Black", text=lst[c-1], tags="numbers")
             #print(canvas1.gettags(t_id))
             text_id.append(t_id)
-            nlist.append(lst[c-1])
+            clicked_num_lst.append(lst[c-1])
             state +=1
             exposed[c-1] = True
-            x.append(c-1)
-        if state == 2:
-            turns += 1
-            label_turns.config(text = "Turns = {} ".format(turns))
-
-        if state == 3 and  nlist[0] == nlist[1] :
-            del text_id[0:2]
-            del nlist[0:2]
-            del x[0:2]
-            state = 1
-
-        if state == 3 and nlist[0] != nlist[1] :
-            exposed[x[0]]= False
-            exposed[x[1]] = False
-            del x[0:2]
-            canvas1.delete(text_id[0])
-            canvas1.delete(text_id[1])
-            del text_id[0:2]
-            del nlist[0:2]
-            state = 1
+            clicked_num_index_lst.append(c-1)
+            if state == 2:
+                turns += 1
+                label_turns.config(text = "Turns = {} ".format(turns))
+            elif state == 3 and  clicked_num_lst[0] == clicked_num_lst[1] :
+                del text_id[0:2]
+                del clicked_num_lst[0:2]
+                del clicked_num_index_lst[0:2]
+                state = 1
+            elif state == 3 and clicked_num_lst[0] != clicked_num_lst[1] :
+                exposed[clicked_num_index_lst[0]]= False
+                exposed[clicked_num_index_lst[1]] = False
+                del clicked_num_index_lst[0:2]
+                canvas1.delete(text_id[0])
+                canvas1.delete(text_id[1])
+                del text_id[0:2]
+                del clicked_num_lst[0:2]
+                state = 1
     except:
-        pass  # This portion is executed when you click on the already displayed number on the card.
+        # This portion is executed when you click on the already displayed number on the card.
+        pass
 
 
 canvas1.bind("<Button-1>", click)
